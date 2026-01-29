7e9efa9deb70b236fbcdab16d3fc7b48fb696035
sajidalhafiz-dev <sajidalhafiz.dev@gmail.com>
2026-01-29 17:09:18 +0600

    connected with prisma

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import app from "./app.js"
+import { prisma } from "./lib/prisma.js"
 
 const port = process.env.PORT || 5000
 
-function server() {
+async function server() {
+
+    try {
+        await prisma.$disconnect()
+    } catch (error) {
+        console.error(error)
+        await prisma.$disconnect()
+        process.exit(1)
+    }
     app.listen(port, () => {
         console.log(`Server is running on PORT:${port}`)
     })
